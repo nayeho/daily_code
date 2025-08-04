15e6ff3be32010592b30929569cc1aa129e804d5
Yeho Na <nayeho@gmail.com>
2025-08-04 16:29:04 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,19 +1,38 @@
-class Casually_Stack:
+# Node 클래스
+class Node: 
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
     def __init__(self):
-        self.s = []
+        self.top = None
+    # push
     def push(self, data):
-        self.s.append(data)
-    def pop(self):
-        return self.s.pop()
-    def is_empty(self):
-        return len(self.s) == 0
-    def peek(self):
-        if self.is_empty() == 1:
-            return "비어 있습니다"
+        if self.top == None: 
+            self.top = Node(data)  # 비어 있다면 새로운 노드를 만들어서 가리키게 하자
         else:
-            return self.s[-1]
-    def __str__(self):
-        return str(self.s)
+            curr = Node(data)    # 비어 있지 않다면 해당 data를 가진 Node를 만들고
+            curr.next = self.top # 만들어진 노드의 next에 원래 top을 가리키게!!
+            self.top = curr      # top을 방금 만들어진 node를 가리키게!!
+    # pop
+    def pop(self):
+        if self.top == None:
+            return None
+        else:
+            curr = self.top
+            self.top = self.top.next
+            return curr.data
+    # peek
+    def peek(self):
+        if self.top == None:
+            return None
+        else:
+            curr = self.top
+            return curr.data
+    # is_empty
+    def is_empty(self):
+        return self.top == None
 
 T = int(input())
 b_list = []
@@ -21,7 +40,7 @@ for i in range(T):
     b_list.append(input())
 
 for test_case in b_list:
-    b_stack = Casually_Stack()
+    b_stack = Stack()
     
     is_valid = True
     
